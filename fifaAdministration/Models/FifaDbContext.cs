c262cb947050fdbe5359fb830243dc0aac3b1dc0
mark.heimer <mark.heimer@t2-informatik.de>
2019-10-02 16:29:25 +0200

    Add project files.

---
+++ fifaAdministration/Models/FifaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fifaAdministration.Models
+{
+    public class FifaDbContext : DbContext
+    {
+        public FifaDbContext(DbContextOptions<FifaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FifaDbContext> FifaItems { get; set; }
+    }
+}
---
+++ fifaAdministration/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace fifaAdministration.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Home { get; set; }
+        public string Away { get; set; }
+        public int HomeGoals { get; set; }
+        public int AwayGoals { get; set; }
+    }
+}
